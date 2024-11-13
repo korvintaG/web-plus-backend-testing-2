cd34c9958ab555cdba0087a88935bce755aba21b
Sergey Korobok <korvin@kati.ru>
2024-11-13 09:49:50 +0300

    Review res

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -21,12 +21,47 @@ describe('PostsService', () => {
 
     it('should return all posts if called without options', () => {
       // реализуйте тест-кейс
+      const foundPosts=postsService.findMany();
+      expect(foundPosts.length).toEqual(posts.length);  
+      expect(foundPosts).toEqual(
+        expect.arrayContaining(posts.map(expect.objectContaining)), 
+      );      
     });
 
     it('should return correct posts for skip and limit options', () => {
       // реализуйте тест-кейс
+      const testSkip=1;
+      const testLimit=2;
+      let foundPosts=postsService.findMany({limit:testLimit, skip: testSkip});
+      expect(foundPosts.length).toEqual(testLimit);  
+      expect(foundPosts).toEqual(
+        expect.arrayContaining(
+          posts.slice(testSkip, testLimit).map(expect.objectContaining),
+        ),
+      );      
     });
 
     // реализуйте недостающие тест-кейсы
+    it('should return correct posts for skip options', () => {
+      const testSkip=2;
+      let foundPosts=postsService.findMany({skip: testSkip});
+      expect(foundPosts.length).toEqual(posts.length-testSkip);  
+      expect(foundPosts).toEqual(
+        expect.arrayContaining(
+          posts.slice(testSkip).map(expect.objectContaining),
+        ),
+      );      
+    });
+
+    it('should return correct posts for limit option', () => {
+      const testLimit = 1;
+      const foundPosts = postsService.findMany({ limit: testLimit });
+      expect(foundPosts).toHaveLength(testLimit);
+      expect(foundPosts).toEqual(
+        expect.arrayContaining(
+          posts.slice(0, testLimit).map(expect.objectContaining),
+        ),
+      );
+    });    
   });
 });
